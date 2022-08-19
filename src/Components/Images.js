b735b3e98072f629fb78d2f02c40d10eae9b845d
Monika Mona <mona.mt.89@gmail.com>
2022-08-19 10:26:23 +0200

    add images hala kwiaty

---
+++ src/Components/Images.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Images () {
+    return (
+        <>
+        <div className="images-container">
+            <h3>tu będą obrazki</h3>
+            <img src="../Images/images" alt="hala"></img>
+        </div>
+        </>
+    )
+}
+export default Images
