4daf8b8b4a5692a8a699d38e2c3719de6c65d75c
Monika Mona <mona.mt.89@gmail.com>
2022-08-20 14:52:00 +0200

    add images TatraMountain Holiday Sun

--- src/Images.js
+++ src/Images.js
@@ -21,11 +21,35 @@ function Images() {
           </li>
         </ul>
         <ul>
-          <li>Image3</li>
+          <img
+            src={require('./images/IMG_2722.JPEG')}
+            alt="hala fiolet"
+            width={500}
+          ></img>
+          <li>
+            <img
+              src={require('./images/IMG_2941.JPEG')}
+              alt="hala zieleń"
+              width={500}
+            ></img>
+          </li>
         </ul>
         <ul>
-          <li>Image1</li>
-          <li>Image1</li>
+          <li>
+            <img
+              src={require('./images/IMG_3100.JPEG')}
+              alt="hala zieleń"
+              width={500}
+            ></img>
+            Image1
+          </li>
+          <li>
+            <img
+              src={require('./images/IMG_3160.JPEG')}
+              alt="hala zieleń"
+              width={500}
+            ></img>
+          </li>
         </ul>
       </section>
     </>
